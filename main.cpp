8ac22ca928c6f9581f716324ee62c907f060db1c
gamekersol <121124234+gamekersol@users.noreply.github.com>
2025-03-15 15:35:47 +0200

    15.03.25

--- main.cpp
+++ main.cpp
@@ -1,37 +1,46 @@
-#include <SDL2/SDL.h>
+#include <SDL3/SDL.h>
+#include <iostream>
+
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main (int argc, char ** args) {
-    
-    if( SDL_Init( SDL_INIT_EVERYTHING ) != 0 )
-    {
+int main(int argc, char** args) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
         return 1;
     }
 
-    SDL_Surface* screen_surface = NULL;
-
-    SDL_Window* window = NULL;
-
-    window = SDL_CreateWindow("Hello, SDL 2!",SDL_WINDOWPOS_UNDEFINED, 
-    SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 
-    SDL_WINDOW_SHOWN);
-    
-    if (window == NULL) {
+    SDL_Window* window = SDL_CreateWindow("Hello, SDL 3!", SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    if (!window) {
+        std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
         return 1;
     }
 
-    screen_surface = SDL_GetWindowSurface(window);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+    if (!renderer) {
+        std::cerr << "Renderer could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
 
-    SDL_FillRect(screen_surface, NULL, SDL_MapRGB( screen_surface->format, 0, 255, 0));
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
 
-    SDL_UpdateWindowSurface(window);
+    SDL_Event e;
+    bool quit = false;
 
-    SDL_Delay(2000);
+    while (!quit) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_EVENT_QUIT) quit = true; // ✅ Виправлено помилку
+        }
+    }
 
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
-
     SDL_Quit();
 
     return 0;
-};
+}
